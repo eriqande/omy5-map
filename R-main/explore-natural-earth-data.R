79ccc98148db97a72675f156b893a90fb4c02d16
Eric C. Anderson <eric.anderson@noaa.gov>
2016-10-17 01:55:02 -0700

    Fiddle around with state and country boundaries.

--- R-main/explore-natural-earth-data.R
+++ R-main/explore-natural-earth-data.R
@@ -26,6 +26,12 @@ usgs_rivers <- readOGR('/Users/eriq/Maps/hydrogm020_nt00015',
 ne_coast <- readOGR("/Users/eriq/Maps/natural-earth-10m/ne_10m_coastline",
                     "ne_10m_coastline")
 
+state_prov <- readOGR("/Users/eriq/Maps/natural-earth-10m/ne_10m_admin_1_states_provinces_lines",
+                      "ne_10m_admin_1_states_provinces_lines")
+
+country_bound <- readOGR("/Users/eriq/Maps/natural-earth-10m/ne_10m_admin_0_boundary_lines_land",
+                         "ne_10m_admin_0_boundary_lines_land")
+
 #### The usgs data set is massive, so we should be able to restrict it to the rivers we want
 #### Here is a test of that
 vent <- usgs_rivers[usgs_rivers$NAME == "Ventura River" & !is.na(usgs_rivers$NAME), ]
@@ -50,16 +56,14 @@ lakes.subset <- quick.subset(ne_lakes, domain)
 river.subset <- quick.subset(ne_rivers, domain)
 coast.subset <- quick.subset(ne_coast, domain)
 vent.subset <- quick.subset(vent, domain)
+state.subset <- quick.subset(state_prov, domain)
+country.subset <- quick.subset(country_bound, domain)
 
 nat.crop <- crop(nat.earth, y=extent(domain))
 
 rast.table <- data.frame(xyFromCell(nat.crop, 1:ncell(nat.crop)),
                          getValues(nat.crop/255))
 
-#rast.table$rgb <- with(rast.table, rgb(NE2_50M_SR_W.1,
-#                                       NE2_50M_SR_W.2,
-#                                       NE2_50M_SR_W.3,
-#                                       1))
 
 rast.table$rgb <- with(rast.table, rgb(HYP_LR_SR_W_DR.1,
                                        HYP_LR_SR_W_DR.2,
@@ -71,7 +75,8 @@ rast.table$rgb <- with(rast.table, rgb(HYP_LR_SR_W_DR.1,
 samps <- read_excel("data/Omy5survey_table_LC.xlsx") %>%
   mutate(lat = as.numeric(Lat),
          long = as.numeric(Long)) %>%
-  filter(!is.na(lat))
+  filter(!is.na(lat)) %>%
+  mutate(Number = paste(1:nrow(.)))
 
 
 
@@ -81,11 +86,14 @@ g <- ggplot(data = rast.table, aes(x = x, y = y)) +
   geom_raster(fill = rast.table$rgb, interpolate = TRUE) +
   geom_polygon(data=lakes.subset, aes(x = long, y = lat, group = group), fill = '#ADD8E6') +
   scale_alpha_discrete(range=c(1,0)) +
+  geom_path(data=state.subset, aes(x = long, y = lat, group = group), color = 'gray30') +
+  geom_path(data=country.subset, aes(x = long, y = lat, group = group), color = 'gray30') +
   geom_path(data=lakes.subset, aes(x = long, y = lat, group = group), color = 'blue') +
   geom_path(data=river.subset, aes(x = long, y = lat, group = group), color = 'blue') +
   geom_path(data=coast.subset, aes(x = long, y = lat, group = group), color = 'blue', size = 0.1) +
-  geom_path(data=vent.subset, aes(x = long, y = lat, group = group), color = 'blue') +
-  geom_point(data = samps, aes(x = long, y = lat)) +
+  geom_path(data=vent.subset, aes(x = long, y = lat, group = group), color = 'blue', size = 0.12) +
+#  geom_point(data = samps, aes(x = long, y = lat)) +
+  geom_text(data = samps, aes(x = long, y = lat, label = Number), size = 0.4) +
   scale_x_continuous(expand=c(0,0)) +
   scale_y_continuous(expand=c(0,0)) +
   xlab('') + ylab('')
